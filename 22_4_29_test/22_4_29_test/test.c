400a0ae1f483911ed8325d21f6ae59f53cd0dd2c
Man9OooO <Man9OooO>
2022-05-05 13:40:03 +0800

    test

--- 22_4_29_test/22_4_29_test/test.c
+++ 22_4_29_test/22_4_29_test/test.c
@@ -1,4 +1,6 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
 #define _CRT_SECURE_NO_WARNINGS 1
 //#include <stdio.h> 
 //int main()
@@ -55,4 +57,62 @@ int main
 
     return 0;
 >>>>>>> 2c28fb84b1fb6bdd2fc90928c51d261fa0f11661
+=======
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h> 
+//int main()
+//{
+//    float lc, cf;
+//    scanf("%f", &lc);
+//    if (lc <= 3)
+//    {
+//        cf = 10;
+//    }
+//    else if (lc > 3 && lc <= 5)
+//    {
+//        cf = 10 + (lc - 3) * 1.8;
+//    }
+//        
+//    else if(lc > 5)
+//    {
+//        cf = 10 + (lc - 3) * 1.8 + (lc - 5) * 2.7;
+//    }
+//   
+ /*   printf("%.2f", cf);
+    return 0;
+}*/
+int main
+{
+
+    return 0;
+>>>>>>> 374fdf0d10971997db2f875470647e4dbfd1651d
+=======
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h> 
+//int main()
+//{
+//    float lc, cf;
+//    scanf("%f", &lc);
+//    if (lc <= 3)
+//    {
+//        cf = 10;
+//    }
+//    else if (lc > 3 && lc <= 5)
+//    {
+//        cf = 10 + (lc - 3) * 1.8;
+//    }
+//        
+//    else if(lc > 5)
+//    {
+//        cf = 10 + (lc - 3) * 1.8 + (lc - 5) * 2.7;
+//    }
+//   
+ /*   printf("%.2f", cf);
+    return 0;
+}*/
+int main
+{
+
+    return 0;
+>>>>>>> 374fdf0d10971997db2f875470647e4dbfd1651d
 }
