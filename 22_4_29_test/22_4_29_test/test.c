78b5f9218e3d9ea46db5a72e467934251662a97b
man9o <321941904@qq.com>
2022-05-05 13:25:31 +0800

    test

---
+++ 22_4_29_test/22_4_29_test/test.c
@@ -0,0 +1,58 @@
+<<<<<<< HEAD
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h> 
+//int main()
+//{
+//    float lc, cf;
+//    scanf("%f", &lc);
+//    if (lc <= 3)
+//    {
+//        cf = 10;
+//    }
+//    else if (lc > 3 && lc <= 5)
+//    {
+//        cf = 10 + (lc - 3) * 1.8;
+//    }
+//        
+//    else if(lc > 5)
+//    {
+//        cf = 10 + (lc - 3) * 1.8 + (lc - 5) * 2.7;
+//    }
+//   
+ /*   printf("%.2f", cf);
+    return 0;
+}*/
+int main
+{
+
+    return 0;
+=======
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h> 
+//int main()
+//{
+//    float lc, cf;
+//    scanf("%f", &lc);
+//    if (lc <= 3)
+//    {
+//        cf = 10;
+//    }
+//    else if (lc > 3 && lc <= 5)
+//    {
+//        cf = 10 + (lc - 3) * 1.8;
+//    }
+//        
+//    else if(lc > 5)
+//    {
+//        cf = 10 + (lc - 3) * 1.8 + (lc - 5) * 2.7;
+//    }
+//   
+ /*   printf("%.2f", cf);
+    return 0;
+}*/
+int main
+{
+
+    return 0;
+>>>>>>> 2c28fb84b1fb6bdd2fc90928c51d261fa0f11661
+}
