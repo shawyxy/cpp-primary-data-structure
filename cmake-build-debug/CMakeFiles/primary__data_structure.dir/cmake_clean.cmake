1b87cba631313e10805aaff72f019c8eb2b4883e
Man9OooO <Man9OooO>
2022-05-05 23:20:39 +0800

    test

---
+++ cmake-build-debug/CMakeFiles/primary__data_structure.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/primary__data_structure.dir/Test_22_5_4/test.c.o"
+  "CMakeFiles/primary__data_structure.dir/Test_22_5_4/test.c.o.d"
+  "primary__data_structure"
+  "primary__data_structure.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/primary__data_structure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
