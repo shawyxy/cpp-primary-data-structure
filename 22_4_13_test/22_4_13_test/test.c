772100141e12f543cae94504e96a3576b577ccfc
Man9OooO <Man9OooO>
2022-05-05 10:27:49 +0800

    test

---
+++ 22_4_13_test/22_4_13_test/test.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 0, b = 0;
+//	while (~scanf("%d %d", &a, &b))
+//	{
+//		printf("%d\n", a + b);
+//	}
+//	return 0;
+//}
