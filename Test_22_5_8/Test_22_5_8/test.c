302032aa9c001f487d130dc3ec38e463f3b8a3ba
man9o <321941904@qq.com>
2022-05-08 10:13:28 +0800

    test

---
+++ Test_22_5_8/Test_22_5_8/test.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	int num = 1;
+//	int sum = 0;
+//	for (int i = 1; i <= 5; i++)
+//	{
+//		num *= i;
+//		sum += num;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
