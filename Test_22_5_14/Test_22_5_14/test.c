7b441d7f82322f72b78caba5913a7fd411485a01
Man9OooO <Man9OooO>
2022-05-28 16:43:58 +0800

    Stack

--- Test_22_5_14/Test_22_5_14/test.c
+++ Test_22_5_14/Test_22_5_14/test.c
@@ -1,15 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-int Add(int x, int y)
+//int Add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = Add(a, b);
+//	return 0;
+//}
+void print(int* arr)
 {
-	int z = x + y;
-	return z;
+	for (int i = 0; i < 3; i++)
+	{
+		printf("%d ", arr[i]);
+	}
 }
 
 int main()
 {
-	int a = 10;
-	int b = 20;
-	int c = Add(a, b);
+	int arr[3] = { 1,2,3 };
+	print(arr);
 	return 0;
 }
