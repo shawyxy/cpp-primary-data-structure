b72b4a8b2204a033893b0b3d5ac83786d3908ea5
shawyxy <9inghuan@gmail.com>
2022-06-10 21:41:37 +0800

    spotify

--- test_22_6_9/test_22_6_9/test.c
+++ test_22_6_9/test_22_6_9/test.c
@@ -22,8 +22,38 @@ void test1()
 	HeapPrint(&hp);
 	HeapDestory(&hp);
 }
+void ArrayPrint(int* a)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d ", a[i]);
+	}
+	printf("\n");
+}
+void HeapSort(int* a, int n)
+{
+	for (int i = (n - 1 - 1) / 2; i >= 0; --i)
+	{
+		AdjustDown(a, n, i);
+	}
+
+	int end = n - 1;
+	while (end > 0)
+	{
+		Swap(&a[0], &a[end]);
+		AdjustDown(a, end, 0);
+		--end;
+	}
+}
+
+void TestHeapSort()
+{
+	int a[] = { 27, 15, 19, 18, 28, 34, 65, 49, 25, 37 };
+	HeapSort(a, sizeof(a) / sizeof(int));
+}
 int main()
 {
-	test1();
+	TestHeapSort();
+	
 	return 0;
 }
